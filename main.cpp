83203e61b6003aaf0224ed18eb281c3c34cd6dda
Kevin Liao <liaok0082@gmail.com>
2022-04-29 12:01:27 +0800

    Factorial bug solved

--- main.cpp
+++ main.cpp
@@ -98,13 +98,15 @@ void printInstructions()
     cout << "-----------------------------------------------------------------\n";
     cout << "Instructions: \n";
     cout << "\t1. Make operations: Insert the operation directly.\n";
-    cout << "\t2. Create a variable: Set + datatype + operation.\n";
+    cout << "\t2. Create a variable: Set datatype varName = operation.\n";
     cout << "\t3. List all variables: Variables\n";
     cout << "\t4. Finish program: ctrl + d\n\n";
-    cout << "Reference\n";
+    cout << "References:\n";
     cout << "datatypes: Integer or Decimal.\n";
     cout << "           variable names can only be one character.\n";
     cout << "operators: + - * / ! ^\n";
+    cout << "\nNotes:\n";
+    cout << "All numbers are treated as Decimal.\n";
     cout << "-----------------------------------------------------------------\n\n";
 }
 
@@ -241,8 +243,14 @@ string processStringInput(string input)
 
             parenthesis_index.pop_back();
             parenthesis_stack.pop_back();
+
+            // cout << "[log]: Parenthesis solved.\n";
+            // cout << input << endl;
         }
     }
+
+    // cout << "[log]: Starting arithmetic operations.\n";
+    // cout << input << endl;
 
     // At this step, all parenthesis are removed
     // Detect factorials
@@ -259,9 +267,10 @@ string processStringInput(string input)
             s += input[i];
             string del = "";
             del += Decimal::fraction_delimiter;
-            if (!checkElementInVector(SPECIAL_SYMBOLS, s))
+            if (!checkElementInVector(SPECIAL_SYMBOLS, s) && s != del)
             {
                 cout << "[Error]: Invalid input (invalid characters detected).\n";
+                cout << "Invalid character: " << s << endl;
                 return "";
             }
             else if (checkElementInVector(SPECIAL_SYMBOLS, s) && s != DOT_SIGN && s != del)
